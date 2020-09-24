5df7da2896a39fbcad349a3afe3d2fe38291dc94
kevin <7318377kevintirta@gmail.com>
2020-09-24 15:45:04 +0800

    undo text styling

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { AppLoading } from "expo";
-import { Container, Text } from "native-base";
-import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
-import { useFonts } from "@use-expo/font";
+
 import { HomeScreen, RoomScreen } from "HomeAutomation/src/screens";
 import { StyleSheet, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -13,30 +9,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [isAppReady, setReady] = useState(false);
-
-  useEffect(() => {
-    const loadFont = async () => {
-      await Font.loadAsync({
-        Roboto: require("native-base/Fonts/Roboto.ttf"),
-        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-        ...Ionicons.font,
-      });
-      setReady(true);
-    };
-    loadFont();
-  });
-  if (!isAppReady) {
-    return <AppLoading />;
-  }
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Room" component={RoomScreen} />
       </Stack.Navigator>
-      <Text>testigndsfsd</Text>
     </NavigationContainer>
   );
 }
@@ -47,6 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    fontFamily: "Roboto_medium",
   },
 });
