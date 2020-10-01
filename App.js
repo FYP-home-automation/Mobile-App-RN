ded8e9874e1a5ea62b5b523a68ed93228d7c96ae
kevin <7318377kevintirta@gmail.com>
2020-10-01 10:50:43 +0800

    add usage graph

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 
-import { HomeScreen, RoomScreen } from "HomeAutomation/src/screens";
+import {
+  HomeScreen,
+  RoomScreen,
+  UsageGraphScreen,
+} from "HomeAutomation/src/screens";
 import { StyleSheet, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -14,6 +18,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Room" component={RoomScreen} />
+        <Stack.Screen name="Usage Graph" component={UsageGraphScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,5 @@
 import HomeScreen from "./HomeScreen";
 import RoomScreen from "./RoomScreen";
+import UsageGraphScreen from "./UsageGraphScreen";
 
-export { HomeScreen, RoomScreen };
+export { HomeScreen, RoomScreen, UsageGraphScreen };
