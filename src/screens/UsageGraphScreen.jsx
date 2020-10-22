992949f845485f9f3a0f28e85c561c96018f5e02
kevin gondokusumo <7318377kevintirta@gmail.com>
2020-10-22 11:41:50 +0800

    setup basic redux store

--- src/screens/UsageGraphScreen.jsx
+++ src/screens/UsageGraphScreen.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
+import { connect } from 'react-redux'
+import {setNumber} from '../redux/actions/sampleAction'
 import { StyleSheet, Text, View, Button } from "react-native";
 
-const UsageGraphScreen = () => {
+const UsageGraphScreen = ({onClick, number}) => {
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Usage Graph Screen</Text>
+      <Button title="testing" onPress={()=>onClick( number+1)}></Button>
+      <Text>{number}</Text>
     </View>
   );
 };
 
-export default UsageGraphScreen;
+const mapStateToProps = (state, ownProps) => ({
+  number: state.sample.count
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: (count) => dispatch(setNumber(count))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsageGraphScreen)
