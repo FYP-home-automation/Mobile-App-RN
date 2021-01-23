2ab29dbf912346908b4e5046ee1be4e39e435d30
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-01-23 15:01:20 +0800

    make dynamic devices view

--- src/containers/RoomActiveDevices/RoomActiveDevices.js
+++ src/containers/RoomActiveDevices/RoomActiveDevices.js
@@ -3,43 +3,10 @@ import React, { useState } from 'react';
 import { DeviceBox } from 'HomeAutomation/src/components';
 import { StyleSheet, Text, View } from 'react-native';
 
-const initialDummyData = [
-  {
-    type: 'ios-trash',
-    name: 'light 1',
-    on: false,
-    id: 1,
-  },
-  {
-    type: 'paper-plane',
-    name: 'trashbin',
-    on: true,
-    id: 2,
-  },
-  {
-    type: 'ios-bulb',
-    name: 'printer',
-    on: true,
-    id: 3,
-  },
-  {
-    type: 'tv',
-    name: 'tv',
-    on: true,
-    id: 4,
-  },
-  {
-    type: 'ios-bulb',
-    name: 'light 2',
-    on: true,
-    id: 5,
-  },
-];
-
 const RoomActiveDevices = ({ room }) => {
-  const [dummyData, setDummyData] = useState(initialDummyData);
-  // console.log('room testing', room);
-  if (!Boolean(room)) {
+  const devices = room.gateways.map(gateway => gateway.devices);
+
+  if (devices.length <= 0) {
     return null;
   }
 
@@ -47,18 +14,16 @@ const RoomActiveDevices = ({ room }) => {
     <View style={styles.container}>
       <Text style={styles.title}>5 Active Devices</Text>
       <View style={styles.deviceBoxContainer}>
-        {dummyData.map((data, id) => (
+        {devices[0].map((device, id) => (
           <View>
             <DeviceBox
-              name={data.name}
-              type={data.type}
-              on={data.on}
+              name={device.name}
+              type={device.device_type}
+              on={true}
               onPress={() => {
-                console.log(id);
-                // const targetIdx = dummyData.findIndex(device => device.id == id)
-                const newData = [...dummyData];
-                newData[id].on = !newData[id].on;
-                setDummyData(newData);
+                // const newData = [...dummyData];
+                // newData[id].on = !newData[id].on;
+                // setDummyData(newData);
               }}
             />
           </View>
--- src/utils/global.js
+++ src/utils/global.js
@@ -1,3 +1,6 @@
+import livingRoom from 'HomeAutomation/src/assets/livingRoom.png';
+import kitchen from 'HomeAutomation/src/assets/kitchen.png';
+
 export const capitalize = string => string[0].toUpperCase() + string.slice(1);
 
 export const statsSymbolMappper = {
@@ -6,8 +9,22 @@ export const statsSymbolMappper = {
   brightness: 'lux',
 };
 
+export const roomTypeImageMapper = {
+  'Living Room': livingRoom,
+  Kitchen: kitchen,
+  Bedroom: null,
+  None: null,
+};
+
 export const statsIconNameMapper = {
   temp: 'thermometer',
   humidity: 'water',
   brightness: 'md-flashlight',
+};
+
+export const deviceIconNameMapper = {
+  Trashbin: 'ios-trash',
+  Lamp: 'ios-bulb',
+  Tv: 'tv',
+  Printer: 'md-print',
 };
