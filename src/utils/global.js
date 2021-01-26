eb80afcdf98954d4388521eacecfad572f98ca2c
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-01-26 17:14:26 +0800

    add and set dynamic room image

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,5 +1,8 @@
 import livingRoom from 'HomeAutomation/src/assets/livingRoom.png';
 import kitchen from 'HomeAutomation/src/assets/kitchen.png';
+import bathroom from 'HomeAutomation/src/assets/bathroom.jpg';
+import bedroom from 'HomeAutomation/src/assets/bedroom.jpg';
+import room from 'HomeAutomation/src/assets/room.jpg';
 
 export const capitalize = string => string[0].toUpperCase() + string.slice(1);
 
@@ -12,8 +15,9 @@ export const statsSymbolMappper = {
 export const roomTypeImageMapper = {
   'Living Room': livingRoom,
   Kitchen: kitchen,
-  Bedroom: null,
-  None: null,
+  Bedroom: bedroom,
+  Bathroom: bathroom,
+  None: room,
 };
 
 export const statsIconNameMapper = {
