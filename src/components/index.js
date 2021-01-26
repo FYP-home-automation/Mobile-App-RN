7d81b97e94e92929afab4ee9c9bff73d3187a0cf
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-01-26 12:27:33 +0800

    make room modal skeleton

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-import CircleIcon from "./CircleIcon/CircleIcon";
-import CircleStats from "./CircleStats/CircleStats";
-import DeviceBox from "./DeviceBox/DeviceBox";
+import CircleIcon from './CircleIcon/CircleIcon';
+import CircleStats from './CircleStats/CircleStats';
+import DeviceBox from './DeviceBox/DeviceBox';
+import RoomModal from './Modals/RoomModal';
 
-export { CircleIcon, CircleStats, DeviceBox };
+export { CircleIcon, CircleStats, DeviceBox, RoomModal };
