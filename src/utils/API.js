c75246570d302034bac16be029c2548b7217b40c
kevin gondokusumo <kevingondokusumo@kevins-MacBook-Pro.local>
2021-01-26 16:37:10 +0800

    change api axios timeout duration

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: "https://thawing-inlet-42519.herokuapp.com/api",
-  timeout: 6000,
+  baseURL: 'https://thawing-inlet-42519.herokuapp.com/api',
+  timeout: 100000,
 });
