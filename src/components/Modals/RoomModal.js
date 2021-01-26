9acd421ae93efb8546ae07b82d9ea9ee77d30369
kevin gondokusumo <kevingondokusumo@kevins-MacBook-Pro.local>
2021-01-26 16:38:06 +0800

    make add room modal view

--- src/components/Modals/RoomModal.js
+++ src/components/Modals/RoomModal.js
@@ -6,21 +6,89 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
+  TextInput,
 } from 'react-native';
 
+import DropDownPicker from 'react-native-dropdown-picker';
+
+const defaultRoom = 'Living Room';
+
 const RoomModal = ({ modalVisible, onClose }) => {
+  const [roomName, setRoomName] = useState(null);
+  const [chosenRoom, setChosenRoom] = useState(defaultRoom);
+
+  const pressSubmit = () => {
+    setRoomName(null);
+    setChosenRoom(defaultRoom);
+    onClose(!modalVisible);
+  };
+
+  const pressCancel = () => {
+    setRoomName(null);
+    setChosenRoom(defaultRoom);
+    onClose(!modalVisible);
+  };
+
+  const roomOptions = [
+    {
+      label: 'Living Room',
+      value: 'Living Room',
+    },
+    {
+      label: 'Kitchen',
+      value: 'Kitchen',
+    },
+    {
+      label: 'Bedroom',
+      value: 'Bedroom',
+    },
+    {
+      label: 'Others',
+      value: 'Others',
+    },
+  ];
+
   return (
     <View style={styles.centeredView}>
       <Modal animationType="slide" transparent={true} visible={modalVisible}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
-              onPress={() => onClose(!modalVisible)}
-            >
-              <Text style={styles.textStyle}>Hide Modalsd</Text>
-            </TouchableHighlight>
+            <Text style={styles.modalTitle}>Add New Room</Text>
+            <View style={styles.optionContainer}>
+              <Text style={styles.modalOptionTitle}>Room Name</Text>
+              <TextInput
+                style={styles.roomName}
+                onChangeText={text => setRoomName(text)}
+                value={roomName}
+                placeholder="input name"
+              />
+            </View>
+            <View style={styles.optionContainer}>
+              <Text style={styles.modalOptionTitle}>Room Type</Text>
+              <DropDownPicker
+                items={roomOptions}
+                defaultValue={chosenRoom}
+                containerStyle={styles.dropdownContainer}
+                itemStyle={styles.dropdownItem}
+                labelStyle={styles.dropdownLabel}
+                onChangeItem={item => setChosenRoom(item.value)}
+              />
+            </View>
+
+            <View style={styles.buttonGroup}>
+              <TouchableHighlight
+                style={{ ...styles.button, ...styles.cancelButton }}
+                onPress={() => pressCancel()}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={{ ...styles.button, ...styles.submitButton }}
+                onPress={() => pressSubmit()}
+              >
+                <Text style={styles.textStyle}>Submit</Text>
+              </TouchableHighlight>
+            </View>
           </View>
         </View>
       </Modal>
@@ -29,6 +97,42 @@ const RoomModal = ({ modalVisible, onClose }) => {
 };
 
 const styles = StyleSheet.create({
+  dropdownContainer: {
+    height: 40,
+    width: 125,
+  },
+  dropdownItem: {
+    justifyContent: 'flex-start',
+  },
+  dropdownLabel: {
+    fontSize: 14,
+    color: '#000',
+  },
+  optionContainer: {
+    marginBottom: 22,
+    zIndex: 1,
+  },
+  roomName: {
+    height: 40,
+    borderColor: '#C8C8C8',
+    width: 120,
+    borderWidth: 1,
+    borderRadius: 10,
+    zIndex: 2,
+    padding: 10,
+  },
+  cancelButton: {
+    backgroundColor: '#D11A2A',
+    marginRight: 5,
+  },
+  submitButton: {
+    backgroundColor: '#2196F3',
+    marginLeft: 5,
+  },
+  buttonGroup: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
   centeredView: {
     flex: 1,
     justifyContent: 'center',
@@ -39,7 +143,7 @@ const styles = StyleSheet.create({
     margin: 20,
     backgroundColor: 'white',
     borderRadius: 20,
-    padding: 35,
+    padding: 40,
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: {
@@ -49,21 +153,27 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+    position: 'absolute',
   },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
+  button: {
+    borderRadius: 12,
     padding: 10,
-    elevation: 2,
+    elevation: 5,
   },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  modalText: {
+  modalOptionTitle: {
     marginBottom: 15,
     textAlign: 'center',
+    fontWeight: '500',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
 
