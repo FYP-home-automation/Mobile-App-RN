bcd4de1b99b4cfe4223104b53f298d297519318a
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-01-27 16:06:53 +0800

    make add device UI skeleton

--- src/containers/RoomActiveDevices/RoomActiveDevices.js
+++ src/containers/RoomActiveDevices/RoomActiveDevices.js
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
 
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'native-base';
+
 import { DeviceBox } from 'HomeAutomation/src/components';
 import { StyleSheet, Text, View } from 'react-native';
+import { DeviceModal } from 'HomeAutomation/src/components';
 
 const RoomActiveDevices = ({ room }) => {
   const devices = room.gateways.map(gateway => gateway.devices);
+  const [modalVisible, setModalVisible] = useState(false);
 
   if (devices.length <= 0) {
     return null;
   }
 
+  // TODO: Integrate With Back-end
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>5 Active Devices</Text>
-      <View style={styles.deviceBoxContainer}>
-        {devices[0].map((device, id) => (
-          <View>
+    <View style={styles.flexStyle}>
+      <View style={styles.container}>
+        <View style={styles.roomDeviceTopSection}>
+          <Text style={styles.title}>5 Active Devices</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setModalVisible(true)}
+          >
+            <Icon name="ios-add-circle-outline" style={styles.addButtonIcon} />
+          </TouchableOpacity>
+        </View>
+        <View style={{ ...styles.deviceBoxContainer, ...styles.flexStyle }}>
+          {devices[0].map(device => (
             <DeviceBox
               name={device.name}
               type={device.device_type}
@@ -26,21 +40,42 @@ const RoomActiveDevices = ({ room }) => {
                 // setDummyData(newData);
               }}
             />
-          </View>
-        ))}
+          ))}
+        </View>
       </View>
+      <DeviceModal
+        modalVisible={modalVisible}
+        onClose={state => setModalVisible(state)}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  flexStyle: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  addButtonIcon: {
+    marginRight: 17,
+  },
+  roomDeviceTopSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
   title: {
     fontWeight: '700',
     fontSize: 16,
     marginBottom: 10,
   },
   container: {
-    padding: 20,
+    // padding: 20,
+    display: 'flex',
+    borderColor: 'black',
+    // borderWidth: 1,
+    width: '90%',
+    marginTop: 10,
   },
   deviceBoxContainer: {
     flexDirection: 'row',
