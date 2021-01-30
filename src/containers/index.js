4e1d689a85afd3eae68c2484c21ab98cb1924581
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-01-30 15:57:02 +0800

    make top header for indoor tracking screen

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,13 @@
-import Header from "./Header/Header.js";
-import LivingSpaces from "./LivingSpaces/LivingSpaces.js";
-import RoomStatsBar from "./RoomStatsBar/RoomStatsBar";
-import RoomActiveDevices from "./RoomActiveDevices/RoomActiveDevices";
+import Header from './Header/Header.js';
+import LivingSpaces from './LivingSpaces/LivingSpaces.js';
+import RoomStatsBar from './RoomStatsBar/RoomStatsBar';
+import RoomActiveDevices from './RoomActiveDevices/RoomActiveDevices';
+import IndoorTrackingHeader from './IndoorTrackingHeader/IndoorTrackingHeader';
 
-export { Header, LivingSpaces, RoomStatsBar, RoomActiveDevices };
+export {
+  Header,
+  LivingSpaces,
+  RoomStatsBar,
+  RoomActiveDevices,
+  IndoorTrackingHeader,
+};
