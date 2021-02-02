cca4362725e12c72ec85aaf8a90537e9fb7e0a93
kevin gondokusumo <kevingondokusumo@kevins-MacBook-Pro.local>
2021-02-02 15:13:50 +0800

    change setup and tracking to be container

---
+++ src/containers/IndoorTracking/SetupTab.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const SetupTab = () => <View style={styles.container}></View>;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff4081',
+  },
+});
+
+export default SetupTab;
---
+++ src/containers/IndoorTracking/TrackingTab.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const TrackingTab = () => <View style={styles.container}></View>;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#12d',
+  },
+});
+
+export default TrackingTab;
