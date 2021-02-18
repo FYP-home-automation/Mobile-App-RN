032c6dae39698758a29c581e0dc662fa5ac3df89
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-02-18 17:02:08 +0800

    make loading animation for uploading floor plan (first iteration)

--- src/containers/IndoorTracking/SetupTab.js
+++ src/containers/IndoorTracking/SetupTab.js
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import data from '../../assets/rooms.json';
 import { roomNumColorMapper } from 'HomeAutomation/src/utils/global';
+import { Icon } from 'native-base';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as ImagePicker from 'expo-image-picker';
 
-const SetupTab = () => {
+import AnimatedLoader from 'react-native-animated-loader';
+
+const SetupTab = ({ image, setImage }) => {
   const roomnums = data.roomnums;
   const [colorMapper, setColorMapper] = useState(roomNumColorMapper);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setInterval(() => {
@@ -33,22 +39,90 @@ const SetupTab = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const renderSteps = () => {
+    if (image) {
+      return (
+        <View style={styles.flexRow}>
+          {roomnums.map(roomCol => (
+            <View>
+              {roomCol.map(roomNum => (
+                <View style={styles.room(roomNum, colorMapper)}></View>
+              ))}
+            </View>
+          ))}
+        </View>
+      );
+    }
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          setLoading(true);
+          setTimeout(() => {
+            pickImage();
+          }, 50);
+        }}
+        style={styles.topMargin}
+      >
+        <View style={styles.uploadBox}>
+          <Icon style={styles.uploadIcon} name="md-cloud-upload" />
+          <Text>Upload your Floorplan here</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.flexRow}>
-        {roomnums.map(roomCol => (
-          <View>
-            {roomCol.map(roomNum => (
-              <View style={styles.room(roomNum, colorMapper)}></View>
-            ))}
-          </View>
-        ))}
-      </View>
+      {loading ? (
+        <AnimatedLoader
+          visible={true}
+          overlayColor="rgba(255,255,255,0.75)"
+          source={require('../../assets/48401-ox-lantern-zodiac-lunar-new-year-2021.json')}
+          animationStyle={styles.lottie}
+          speed={1}
+        >
+          <Text>Doing something...</Text>
+        </AnimatedLoader>
+      ) : (
+        renderSteps()
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  lottie: {
+    width: 300,
+    height: 300,
+  },
+  topMargin: {
+    marginTop: 100,
+  },
+  uploadBox: {
+    borderWidth: 2,
+    width: 200,
+    height: 200,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  uploadIcon: {
+    fontSize: 40,
+  },
   flexRow: {
     display: 'flex',
     flexDirection: 'row',
