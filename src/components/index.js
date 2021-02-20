ef3bfc551cd44d9a739f6c98e672532883f14732
kevin gondokusumo <7318377kevintirta@gmail.com>
2021-02-20 16:41:21 +0800

    make room legends for indoor tracking screen

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import CircleStats from './CircleStats/CircleStats';
 import DeviceBox from './DeviceBox/DeviceBox';
 import RoomModal from './Modals/RoomModal';
 import DeviceModal from './Modals/DeviceModal';
+import RoomLegends from './Legends/RoomLegends';
 
-export { CircleIcon, CircleStats, DeviceBox, RoomModal, DeviceModal };
+export {
+  CircleIcon,
+  CircleStats,
+  DeviceBox,
+  RoomModal,
+  DeviceModal,
+  RoomLegends,
+};
